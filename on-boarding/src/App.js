19b3a1faecf0123bd823af2ea46167ede4dd7e24
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-05-12 18:46:18 -0400

    stuff

---
+++ on-boarding/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./component/Form";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
