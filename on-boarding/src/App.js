594090c998fcb220708b3f350167e3944d77f27d
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-05-14 22:09:19 -0400

    holy cow

--- on-boarding/src/App.js
+++ on-boarding/src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./component/Form";
 
-const App = () => {
+function App() {
+  const [users, setUsers] = useState([]);
+
+  // addUser is a function – pass user as a prop
+  const addUser = (user) => {
+    const newUser = {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      terms: false,
+    };
+    setUsers([...users, newUser]);
+  };
+  console.log(users);
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Form />
+      <h1> Welcome! </h1>
+      <Form addUser={addUser} formUsers={users} />
+      {/* <pre>{JSON.stringify(users, null, 2)}</pre> */}
     </div>
   );
-};
+}
 
 export default App;
